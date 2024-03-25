4e32ee27a57a8d982817b1c43bde98cfe238bf11
Adam McKee <adam.be.g84d@gmail.com>
2024-03-25 14:09:10 -0500

    move root css variables into a svelte layout so hmr works

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<slot/>
+
+<style>
+    :root {
+        --header-height: 4rem;
+        --panel-width: calc(100% - var(--page-img-scaled-w));
+        --footer-height: 3rem;
+        --panel-bg-color: rgba(0 0 0 / 93%);
+        --app-ui-z-index: 3;
+        --page-img-z-index: 1;
+    }
+</style>
