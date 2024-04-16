eab3d9ee067f31f3f5651b440749b5ccea35b9a9
Adam McKee <adam.be.g84d@gmail.com>
2024-04-15 19:40:21 -0500

    trying a .svelte.ts hof

--- ui/src/lib/qwerky_app.svelte
+++ ui/src/lib/qwerky_app.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
     import {onMount} from 'svelte'
     import {InspectPoint, OpenPage, type Point, type Size} from '@eighty4/qwerky-contract'
-    import {type BoundingBox, buildBoundingBoxes} from '$lib/data/BoundingBox'
-    import type {InspectResult} from '$lib/data/InspectResult'
     import {QwerkyClient} from '$lib/data/QwerkyClient.js'
     import UrlForm from '$lib/components/url_form.svelte'
+    import {type AppData, createAppData} from '$lib/data/AppData.svelte'
     import Footer from '$lib/footer/qwerky_footer.svelte'
     import Header from '$lib/header/qwerky_header.svelte'
     import Panel from '$lib/panel/app_panel.svelte'
     import PageImage from '$lib/page/open_page.svelte'
 
+    const appData: AppData = createAppData()
     let qwerkyClient: QwerkyClient | null = $state(null)
     let pageLoading: boolean = $state(false)
-    let url: string | null = $state(null)
     let currentPageImage: { base64: string, size: Size } | null = $state(null)
-    let inspectResult: InspectResult | null = $state(null)
-    let boundingBoxes: Array<BoundingBox> | null = $derived.by(() => buildBoundingBoxes(inspectResult?.elements))
 
     onMount(() => QwerkyClient.connect({
         onImageData(base64, size: Size) {
@@ -24,11 +21,11 @@
         },
         onDescribePoint(point, elements) {
             console.log('describe', point, elements)
-            inspectResult = {point, elements}
+            appData.addInspectResult({point, elements})
         },
         onDescribeSelector(selector, elements) {
             console.log('describe', selector, elements)
-            inspectResult = {selector, elements}
+            appData.addInspectResult({selector, elements})
         },
         onConnectionLost() {
             document.body.innerHTML = '<div style="position: fixed; top: 0; left: 0; right: 0; bottom: 0; background: maroon; display: flex; justify-content: center; align-items: center"><span>ERRCONN: reload webpage</span></div>'
@@ -37,31 +34,28 @@
 
     function onUrl(event: CustomEvent<string>) {
         pageLoading = true
-        url = event.detail
-        console.log(JSON.stringify(url))
-        qwerkyClient!.sendMessage(new OpenPage(qwerkyClient!.sessionId, url))
+        appData.url = event.detail
+        qwerkyClient!.sendMessage(new OpenPage(qwerkyClient!.sessionId, event.detail))
     }
 
     function onInspectPoint(event: CustomEvent<Point>) {
-        inspectResult = null
-        console.log(JSON.stringify(event.detail))
         qwerkyClient!.sendMessage(new InspectPoint(qwerkyClient!.sessionId, event.detail))
     }
 </script>
 
 {#if qwerkyClient}
-    {#if !url}
+    {#if !appData.url}
         <UrlForm on:url={onUrl}/>
     {:else if currentPageImage !== null}
-        <PageImage imageBase64={currentPageImage.base64}
+        <PageImage boundingBoxes={appData.focusedInspection?.boundingBoxes}
+                   imageBase64={currentPageImage.base64}
                    imageSize={currentPageImage.size}
-                   boundingBoxes={boundingBoxes}
                    on:inspectPoint={onInspectPoint}/>
     {/if}
 {/if}
 
-<Header pageLoading={pageLoading} url={url}/>
-<Panel inspectResult={inspectResult}/>
+<Header pageLoading={pageLoading} url={appData.url}/>
+<Panel appData={appData}/>
 <Footer/>
 
 <!-- todo offsetting stacked highlights to not overlap requires extending highlight buffer by highlight count -->
