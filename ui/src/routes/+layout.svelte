ca91d078b53e3a675b606e63b8b487699ab038c0
Adam McKee <adam.be.g84d@gmail.com>
2024-04-19 15:45:04 -0500

    layout typing

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+    import type {Snippet} from 'svelte'
     import QwerkyRoot from '$lib/qwerky_root.svelte'
-    let { children } = $props();
+
+    let {children}: { children: Snippet } = $props()
 </script>
 
 <QwerkyRoot>
