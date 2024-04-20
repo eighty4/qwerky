8f14ad61fd98e10879648d1a9b4947def172823c
Adam McKee <adam.be.g84d@gmail.com>
2024-04-20 16:18:51 -0500

    merging layout into app component

--- ui/src/lib/qwerky_app.svelte
+++ ui/src/lib/qwerky_app.svelte
@@ -90,6 +90,19 @@
 <div class="hb r"></div>
 
 <style>
+    :root {
+        --header-height: 4rem;
+        --edge-width: 2rem;
+        --panel-width: max(25vw, 20rem);
+        --footer-height: 4rem;
+        --highlight-width: 3px;
+        --panel-bg-color: rgba(0 0 0 / 93%);
+        --app-ui-z-index: 5;
+        --page-highlight-z-index: 4;
+        --page-img-z-index: 2;
+        --panel-bg-z-index: 1;
+    }
+
     .hb {
         background: var(--panel-bg-color);
         position: fixed;
